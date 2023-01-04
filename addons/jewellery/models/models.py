39d4728a7abfb1bcea8d5ac70be782ffd0f4925c
odooerpumg <mgthuyahtut@gmail.com>
2023-01-04 22:10:10 +0630

    Update
    1. Add new fields into product [total weight, diamon weight] [DONE]
    2. Stones Quantity[New Model/Table with name and quantity fields] [DONE]

--- addons/jewellery/models/models.py
+++ addons/jewellery/models/models.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+
+class JewelPieces(models.Model):
+    _name = 'jewel.pieces'
+    _description = 'The records for pieces of jewel.'
+
+    name = fields.Char(string='Name')
+    quantity = fields.Float(string='Quantity', digits='Product Unit of Measure')
+    template_id = fields.Many2one('product.template',string='Template ID')
 
 
-# class jewellery(models.Model):
-#     _name = 'jewellery.jewellery'
-#     _description = 'jewellery.jewellery'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    total_weight = fields.Float(string='Total Weight', digits='Product Unit of Measure')
+    diamond_weight = fields.Float(string='Diamond Weight', digits='Product Unit of Measure')
+    jewel_pieces_ids = fields.One2many('jewel.pieces', 'template_id', string='Jewel Pieces')
+    
+
